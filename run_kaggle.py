9bcdde3b8605febff8bf9c363fd875756e3ec1bb
David <dmorara@unal.edu.co>
2023-02-12 15:09:19 -0500

    Training logging and running in kaggle
    - Code for logging accuracy(class/mean) and iou(class/mean) during train
      and eval
    - Python script for running training in kaggle
    - Removed Bash scripts

---
+++ run_kaggle.py
@@ -0,0 +1,62 @@
+import kaggle
+import json
+import os
+from pathlib import Path
+
+api = kaggle.api
+api.authenticate()
+username = api.get_config_value(api.CONFIG_NAME_USER)
+src_dir = Path("src")
+datasets = [
+    "balraj98/deepglobe-land-cover-classification-dataset",
+    "davidfmora/processed-masks",
+]
+util_scripts = ["dataset", "model", "utils"]
+
+
+def run_script(script, data=[], scripts=[], gpu=False, util=False):
+    kernels = [f"{username}/{script}" for script in scripts]
+    # kernel metadata
+    metadata = {
+        "id": f"{username}/{script}",
+        "title": script,
+        "code_file": f"{src_dir / script}.py",
+        "language": "python",
+        "kernel_type": "script",
+        "is_private": "true",
+        "keywords": ["util-script"] if util else [],
+        "enable_gpu": gpu,
+        "enable_internet": "true",
+        "dataset_sources": data,
+        "competition_sources": [],
+        "kernel_sources": kernels,
+    }
+    with open("kernel-metadata.json", "w") as file:
+        json.dump(metadata, file)
+
+    # wait for the secondary scripts to run before running the main script
+    if scripts:
+        print("\nWaiting for secondary scripts to finish running.")
+        complete = []
+        while sum(complete) != len(kernels):
+            status = [api.kernels_status(kernel)["status"] for kernel in kernels]
+            complete = ["complete" in st for st in status]
+            failed = ["error" in st for st in status]
+            if sum(failed) > 0:
+                print(
+                    "Scripts failed:",
+                    ",".join([scripts[idx] for idx, fail in enumerate(failed) if fail]),
+                )
+                return
+
+    # push kernel to kaggle
+    api.kernels_push_cli(os.getcwd())
+
+    os.remove("kernel-metadata.json")
+
+
+if __name__ == "__main__":
+    run_script("dataset", data=datasets, util=True)
+    run_script("model", util=True)
+    run_script("utils", util=True)
+    run_script("train", scripts=util_scripts, data=datasets, gpu=True)
--- src/train.py
+++ src/train.py
@@ -1,108 +1,238 @@
 import torch
 import torchvision
+from torchvision.transforms.functional import to_pil_image
 from torch import nn
 from torch.utils.data import DataLoader
-from torchvision.transforms.functional import to_pil_image
-from torchvision.utils import make_grid
-from sklearn.model_selection import train_test_split
 
 import wandb
 
-from dataset import LandcoverDataset, image_ids, class_colors
+from dataset import LandcoverDataset, class_names
 from model import Unet
-from utils import mask_to_img
+from utils import ohe_mask
 
 
+# reproducibility
 torch.manual_seed(1)
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
-LOGGING = True
 
-
-if LOGGING:
-    wandb.login(key="5f5a6e6618ddafd57c6c7b40a8313449bfd7a04e")
-
+# logging
+wandb_log = True
+# data
+resize_red = 512
+batch_size = 4
+epochs = 1
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# split in train and test sets
-train_ids, test_ids = train_test_split(
-    image_ids, train_size=0.8, shuffle=True, random_state=42
-)
+# init model and optimizer
+model = Unet()
+model.to(device)
+optimizer = torch.optim.Adam(model.parameters())
+loss_fn = nn.CrossEntropyLoss()
 
-# training params
-BATCH_SIZE = 4
-EPOCHS = 500
-MODEL = Unet().to(device)
-OPTIMIZER = torch.optim.Adam(MODEL.parameters())
-LOSS_FN = nn.CrossEntropyLoss()
-RESIZE_RES = 512
-
-# datasets
+# init datasets and dataloaders
 train_dataset = LandcoverDataset(
-    train_ids,
-    transform=torchvision.transforms.Resize(RESIZE_RES),
-    target_transform=torchvision.transforms.Resize(RESIZE_RES),
+    train=True,
+    transform=torchvision.transforms.Resize(resize_red),
+    target_transform=torchvision.transforms.Resize(resize_red),
 )
 test_dataset = LandcoverDataset(
-    test_ids,
-    transform=torchvision.transforms.Resize(RESIZE_RES),
-    target_transform=torchvision.transforms.Resize(RESIZE_RES),
+    train=False,
+    transform=torchvision.transforms.Resize(resize_red),
+    target_transform=torchvision.transforms.Resize(resize_red),
+)
+# TODO: Try setting pin_memory to true in order to speed up training
+# TODO: Play around with num_workers
+train_dataloader = DataLoader(
+    train_dataset, batch_size=batch_size, num_workers=0, pin_memory=False, shuffle=True
+)
+test_dataloader = DataLoader(
+    test_dataset, batch_size=batch_size, num_workers=0, pin_memory=False, shuffle=True
 )
 
-# create dataloaders
-train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=False)
-test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
-
 # log training and data config
-if LOGGING:
+if wandb_log:
+    wandb.login(key="5f5a6e6618ddafd57c6c7b40a8313449bfd7a04e")
     wandb.init(
         project="landcover-segmentation",
         config={
-            "epochs": EPOCHS,
-            "batch_size": BATCH_SIZE,
-            "resize_res": RESIZE_RES,
-            "optimizer": type(OPTIMIZER).__name__,
-            "loss_fn": type(LOSS_FN).__name__,
+            "epochs": epochs,
+            "batch_size": batch_size,
+            "resize_res": resize_red,
+            "optimizer": type(optimizer).__name__,
+            "loss_fn": type(loss_fn).__name__,
+            "model": type(model).__name__,
         },
     )
     # record model gradients
-    wandb.watch(MODEL, log_freq=10)
+    wandb.watch(model, log_freq=10)
 
-X, y = next(iter(train_dataloader))
-X, y = X.to(device), y.to(device)
 
-# training loop
-MODEL.train()
-for epoch in range(EPOCHS):
-    # forward pass
-    pred = MODEL(X)
-    loss = LOSS_FN(pred, y)
-    # backward pass
-    OPTIMIZER.zero_grad()
-    loss.backward()
-    OPTIMIZER.step()
+for epoch in range(1, epochs + 1):
+    print(
+        "Epoch {} of {}, {}/{} batches of size {}*{}".format(
+            epoch,
+            epochs,
+            len(train_dataloader),
+            len(test_dataloader),
+            batch_size,
+            (torch.cuda.device_count() if device == "cuda" else 1),
+        )
+    )
 
-    if epoch % 20 == 0:
-        print(f"loss: {loss.detach().item():>7f}  [{epoch:>5d}/{EPOCHS:>5d}]")
+    # training loop
+    class_preds = torch.zeros(7, 7)
+    model.train()
+    for batch, (X, y) in enumerate(train_dataloader):
+        X, y = X.to(device), y.to(device)
+        # forward pass
+        logits = model(X)
+        loss = loss_fn(logits, y)
+        # backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    if LOGGING:
-        metrics = {"train/train_loss": loss.detach().item()}
-        wandb.log(metrics, step=epoch)
+        # calculate predictions per class
+        with torch.no_grad():
+            pred = torch.argmax(logits, 1)
+            ohe_pred = ohe_mask(pred, 7)
+            ohe_y = ohe_mask(y, 7)
+            class_preds += torch.stack(
+                [
+                    (ohe_pred & ohe_y[:, c].unsqueeze(1)).sum(dim=[0, 2, 3])
+                    for c in range(7)
+                ]
+            ).to("cpu")
 
-# predictions
-with torch.no_grad():
-    preds = MODEL(X)
-dis_preds = torch.argmax(preds, 1)
+        if batch % 10 == 0:
+            print(
+                f"loss: {loss.detach().item():>7f}  [{batch:>5d}/{len(train_dataloader):>5d}]"
+            )
+        if wandb_log:
+            wandb.log({"train/loss": loss.detach().item()})
 
-if LOGGING:
-    for i in range(BATCH_SIZE):
-        image = (X[i] * 255).type(torch.uint8).to("cpu")
-        target = mask_to_img(y[i].to("cpu"), class_colors)
-        pred = mask_to_img(dis_preds[i].to("cpu"), class_colors)
-        imgs = make_grid([image, target, pred])
+    if wandb_log:
+        with torch.no_grad():
+            tp = class_preds.diag()
+            fp = class_preds.sum(1) - tp
+            n_class = class_preds.sum(1)
+            accuracy = tp / (n_class + 1e-5)
+            iou = tp / (n_class - tp + fp + 1e-5)
 
+        wandb.log({"epoch": epoch})
+        # log accuracy
         wandb.log(
-            {f"image_{i}": wandb.Image(to_pil_image(imgs), caption="sat/gt/pred")}
+            {
+                "train/mean_accuracy": accuracy.mean().item(),
+                "epoch": epoch,
+            }
+        )
+        wandb.log(
+            {
+                **{
+                    f"train/accuarcy_{c_name}": acc
+                    for c_name, acc in zip(class_names, accuracy.tolist())
+                },
+                **{"epoch": epoch},
+            }
+        )
+        # log iou
+        wandb.log(
+            {
+                "train/mean_iou": iou.mean().item(),
+                "epoch": epoch,
+            }
+        )
+        wandb.log(
+            {
+                **{
+                    f"train/iou_{c_name}": iou
+                    for c_name, iou in zip(class_names, iou.tolist())
+                },
+                **{"epoch": epoch},
+            }
         )
 
+    # TODO: Add validation cadence
+    # validation loop
+    class_preds *= 0
+    val_loss = 0.0
+    model.eval()
+    size = len(test_dataloader)
+    with torch.no_grad():
+        for batch, (X, y) in enumerate(test_dataloader):
+            X, y = X.to(device), y.to(device)
+            # forward pass
+            logits = model(X)
+            loss = loss_fn(logits, y)
+            val_loss += loss.detach().item()
+
+            with torch.no_grad():
+                pred = torch.argmax(logits, 1)
+                ohe_pred = ohe_mask(pred, 7)
+                ohe_y = ohe_mask(y, 7)
+                class_preds += torch.stack(
+                    [
+                        (ohe_pred & ohe_y[:, c].unsqueeze(1)).sum(dim=[0, 2, 3])
+                        for c in range(7)
+                    ]
+                ).to("cpu")
+
+                # log image predictions
+                if wandb_log:
+                    for i in range(len(X)):
+                        image = to_pil_image((X[i] * 255).type(torch.uint8).to("cpu"))
+                        wandb.log(
+                            {
+                                f"Image_{(i + 1) + (batch*batch_size)}": wandb.Image(
+                                    image,
+                                    masks={
+                                        "predictions": {
+                                            "mask_data": pred[i].to("cpu").numpy(),
+                                            "class_labels": {
+                                                idx: name
+                                                for idx, name in enumerate(class_names)
+                                            },
+                                        },
+                                        "ground_truth": {
+                                            "mask_data": y[i].to("cpu").numpy(),
+                                            "class_labels": {
+                                                idx: name
+                                                for idx, name in enumerate(class_names)
+                                            },
+                                        },
+                                    },
+                                )
+                            }
+                        )
+
+    val_loss /= size
+    # TODO: Don't consider the unknown class for evaluation
+    if wandb_log:
+        with torch.no_grad():
+            tp = class_preds.diag()
+            fp = class_preds.sum(1) - tp
+            n_class = class_preds.sum(1)
+            accuracy = tp / (n_class + 1e-5)
+            iou = tp / (n_class - tp + fp + 1e-5)
+
+        # log metrics
+        wandb.summary = {
+            **wandb.summary,
+            "val/mean_loss": val_loss,
+            "val/mean_accuracy": accuracy.mean().item(),
+            **{
+                f"val/accuarcy_{c_name}": acc
+                for c_name, acc in zip(class_names, accuracy.tolist())
+            },
+            "val/mean_iou": iou.mean().item(),
+            **{
+                f"val/iou_{c_name}": iou
+                for c_name, iou in zip(class_names, iou.tolist())
+            },
+        }
+
+
+if wandb_log:
     wandb.finish()
