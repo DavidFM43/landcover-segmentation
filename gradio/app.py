e97d5fc20dd43460064c813e419b78f42998e4a9
santiagoahl <sahumadal@unal.edu.co>
2023-04-11 09:39:02 -0500

    Gradio data exploration

--- gradio/app.py
+++ gradio/app.py
@@ -1,10 +1,17 @@
 import gradio as gr
+import numpy as np
 
 # Build segmentation model
 
+def normalize_img(img, new_shape=(512, 512, 3)):
+    "Normalize a np array in order to have the same Unet's input"
+    return np.resize(img, new_shape=new_shape)
+
 def segment(img):
-    print(type(img))
-    return img
+    img_type = type(img)
+    img_shape = img.shape
+
+    return str(img_shape)
 
 def main():
     # Create the app interface
